582d2dfdebbbd6dc2abd7f9057c03cf939fe0e88
Christian Schenk <cs@miktex.org>
2019-02-24 18:25:17 +0100

    Improvements

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,7 @@ else
     MIKTEX_GID=${MIKTEX_GID:-${MIKTEX_UID}}
     groupadd -g $MIKTEX_GID -o miktex
     useradd --shell /bin/bash -u $MIKTEX_UID -g $MIKTEX_GID -o -c "" -Md /miktex miktex
-    if [ -d /miktex/.miktex ]; then
-        chown -R miktex:miktex /miktex/.miktex
-    fi
+    mkdir -p /miktex/.miktex
+    chown -R miktex:miktex /miktex/.miktex
     exec gosu miktex "$@"
 fi
